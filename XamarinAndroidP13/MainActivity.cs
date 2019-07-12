c441972e69cb09194591ce9192f3e25a9895f75e
isctorres <isctorres@gmail.com>
2019-07-12 16:52:45 -0500

    creacion del repositorio

---
+++ XamarinAndroidP13/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Animation;
+using Android.Views.Animations;
+
+namespace XamarinAndroidP13
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+          
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_practica, menu);
+
+            IMenuItem item = menu.FindItem(Resource.Id.configuracion);
+            View view = LayoutInflater.Inflate(Resource.Layout.layout_menu, null);
+            item.SetActionView(view);
+            var itemView = item.ActionView.JavaCast<Android.Widget.RelativeLayout>();
+
+            /*ObjectAnimator objectAnimator = ObjectAnimator.OfInt(itemView, "width", 300, 250, 300);
+            objectAnimator.SetDuration(3000);
+            objectAnimator.SetInterpolator(new LinearInterpolator());
+            objectAnimator.RepeatCount = 100;
+            objectAnimator.Start();*/
+
+
+            Animation rotation = AnimationUtils.LoadAnimation(ApplicationContext,
+                                               Resource.Animator.rotate_refresh);
+            rotation.RepeatCount = Animation.Infinite;
+            itemView.StartAnimation(rotation);
+            return base.OnCreateOptionsMenu(menu);
+        }
+    }
+}
---
+++ XamarinAndroidP13/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "99bc6dd6-018e-45b9-8e21-94fd1e20934f",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
